f32d0fca9a48f04db83e00c5b3a59261ee8b5485
Jon Gill <jonpgill1@gmail.com>
2021-10-10 16:53:24 +0100

    amend hostname

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //Port and hostname
 const port = process.env.PORT || 3000;
-const hostname = 'https://localhost:9000';
+const hostname = 'http://blogjson-server.herokuapp.com/';
 
 // Set base url to /blogs
 app.get('/', (req, res) => {
